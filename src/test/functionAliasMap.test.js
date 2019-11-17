b468b49a50b2d89533bca9612efcbd4e5141706d
ppiyush13 <ppiyush13@gmail.com>
2019-11-17 19:27:11 +0530

    Adding tests

--- src/test/functionAliasMap.test.js
+++ src/test/functionAliasMap.test.js
@@ -2,12 +2,14 @@ import functionAliasMap from '../funtionAliasMap'
 
 describe('testing functionAliasMap module', () => {
     it('should return alias map', () => {
+        console.log(functionAliasMap)
         expect(functionAliasMap).toEqual({
             equal: [ 'in', 'eq', 'is', 'equal', '=' ],
             notequal: [ 'notin', 'noteq', 'notis', 'notequal', '!=', '<>' ],
             between: [ 'range', 'between' ],
-            dateBetween: ['dateRange', 'dateBetween'],
             notbetween: [ 'notrange', 'notbetween' ],
+            dateBetween: ['dateRange', 'dateBetween'],
+            notdateBetween: [ 'notdateRange', 'notdateBetween' ],
             like: ['like'],
             notlike: ['notlike'],
             greaterThan: [ 'gt', 'greater', 'greaterThan', '>' ],
